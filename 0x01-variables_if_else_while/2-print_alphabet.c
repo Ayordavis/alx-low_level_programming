000c3a9303f8e93dd32b942c5b7e29143e6678b7
Ayordavis <102733939+Ayordavis@users.noreply.github.com>
2022-06-05 23:48:28 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always (Success)
+  *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
 int main(void)
 {
 	char c;
