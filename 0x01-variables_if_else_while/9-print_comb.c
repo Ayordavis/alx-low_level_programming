41c2958b08d417328516cfbbdb6c70ba60ec8fc0
Ayordavis <102733939+Ayordavis@users.noreply.github.com>
2022-06-06 21:18:20 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,15 @@ int main(void)
 	int i = 0;
 
 	for (i = 0; i < 10; ++i)
-	{
+{
 		putchar('0' + i);
 
 		if (i != 9)
-		{
+{
 			putchar(',');
 			putchar(' ');
-		}
-	}
+}
+}
 
 	putchar('\n');
 
