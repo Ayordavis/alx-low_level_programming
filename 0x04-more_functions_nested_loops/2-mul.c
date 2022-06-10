31575a1ce9bd775938a0170d0ba61b26a0fac43e
Ayordavis <ayordaviz@gmail.com>
2022-06-10 06:15:25 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> multiplying two numbers
+ * @a: interger param 1
+ * @b: interger param 2
+ * Return: success and intiger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
