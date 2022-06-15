11456d7d25e5fbc1d4597f56c3c25d95647de347
Ayordavis <ayordaviz@gmail.com>
2022-06-15 03:59:59 -0700

    Task 8

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,21 +15,20 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-	if (i == 0 && str[i] >= 97 && str[i] <= 122)
-	{
-	str[i] = str[i] - 32;
-	}
-	j = 0;
-	while (c[j] != '\0') 
-	        {
-	if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-	{
-	str[i + 1] = str[i + 1] - 32;
-	}
-	j++;
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
 		}
-	i++;
+		j = 0;
+		while (c[j] != '\0')
+				{
+		if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+					{
+						str[i + 1] = str[i + 1] - 32;
+					}
+					j++;
+				}
+		i++;
 	}
 	return (str);
 }
-
