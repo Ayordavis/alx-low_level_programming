93c7c7629b715953c9f64f1c9413554e478c19d4
Ayordavis <ayordaviz@gmail.com>
2022-06-15 05:21:55 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - capitalizes all words of a string
  * @str: the string to change the first letter of a word in uppercase
@@ -15,20 +14,20 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		j = 0;
-		while (c[j] != '\0')
-				{
-		if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-					{
-						str[i + 1] = str[i + 1] - 32;
-					}
-					j++;
-				}
-		i++;
+	if (i == 0 && str[i] >= 97 && str[i] <= 122)
+	{
+	str[i] = str[i] - 32;
+	}
+	j = 0;
+	while (c[j] != '\0')
+	{
+	if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+	{
+	str[i + 1] = str[i + 1] - 32;
+	}
+	j++;
+	}
+	i++;
 	}
 	return (str);
 }
